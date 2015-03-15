c6899af70e009942640ba49ef8c885a07563ce39
eshamster <hamgoostar@gmail.com>
2015-03-15 20:07:05 +0900

    first commit

---
+++ ECS_Proto_First/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Direct3D10;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+
+using AlphaMode = SharpDX.Direct2D1.AlphaMode;
+using Device1 = SharpDX.Direct3D10.Device1;
+using DriverType = SharpDX.Direct3D10.DriverType;
+using Factory = SharpDX.DXGI.Factory;
+using FeatureLevel = SharpDX.Direct3D10.FeatureLevel;
+
+namespace ECS_Proto_First
+{
+    class Game : IDisposable
+    {
+        RenderTargetView renderView;
+        Texture2D backBuffer;
+        Device1 device;
+        SwapChain swapChain;
+        Factory factory;
+
+        public void Run()
+        {
+            var form = new RenderForm("SharpDX - MiniTri Direct2D - Direct3D 10 Sample");
+
+            // SwapChain description
+            var desc = new SwapChainDescription()
+            {
+                BufferCount = 1,
+                ModeDescription =
+                    new ModeDescription(form.ClientSize.Width, form.ClientSize.Height,
+                                        new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                IsWindowed = true,
+                OutputHandle = form.Handle,
+                SampleDescription = new SampleDescription(1, 0),
+                SwapEffect = SwapEffect.Discard,
+                Usage = Usage.RenderTargetOutput
+            };
+
+            // Create Device and SwapChain
+            Device1.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.BgraSupport, desc, FeatureLevel.Level_10_0, out device, out swapChain);
+
+            var d2dFactory = new SharpDX.Direct2D1.Factory();
+
+            int width = form.ClientSize.Width;
+            int height = form.ClientSize.Height;
+
+            var rectangleGeometry = new RoundedRectangleGeometry(d2dFactory, new RoundedRectangle() { RadiusX = 32, RadiusY = 32, Rect = new RectangleF(128, 128, width - 128 * 2, height - 128 * 2) });
+
+            // Ignore all windows events
+            factory = swapChain.GetParent<Factory>();
+            factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAll);
+
+            // New RenderTargetView from the backbuffer
+            backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            renderView = new RenderTargetView(device, backBuffer);
+
+            Surface surface = backBuffer.QueryInterface<Surface>();
+
+
+            var d2dRenderTarget = new RenderTarget(d2dFactory, surface,
+                                                            new RenderTargetProperties(new PixelFormat(Format.Unknown, AlphaMode.Premultiplied)));
+
+            var solidColorBrush = new SolidColorBrush(d2dRenderTarget, Color.White);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Main loop
+            RenderLoop.Run(form, () =>
+            {
+                d2dRenderTarget.BeginDraw();
+                d2dRenderTarget.Clear(Color.Black);
+                solidColorBrush.Color = new Color4(1, 1, 1, (float)Math.Abs(Math.Cos(stopwatch.ElapsedMilliseconds * .001)));
+                d2dRenderTarget.FillGeometry(rectangleGeometry, solidColorBrush, null);
+                d2dRenderTarget.EndDraw();
+
+                swapChain.Present(0, PresentFlags.None);
+            });
+        }
+
+        public void Dispose()
+        {
+            renderView.Dispose();
+            backBuffer.Dispose();
+            device.ClearState();
+            device.Flush();
+            device.Dispose();
+            swapChain.Dispose();
+            factory.Dispose();
+        }
+    }
+}
---
+++ ECS_Proto_First/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS_Proto_First
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
